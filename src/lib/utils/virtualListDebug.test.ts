f41b860bef97bcabcb7a6b2d0a4c88842737ca66
Jason Kummerl <jason.h@kummerl.com>
2025-07-31 17:20:26 -0400

    More cleanups

--- src/lib/utils/virtualListDebug.test.ts
+++ src/lib/utils/virtualListDebug.test.ts
@@ -43,8 +43,8 @@ describe('virtualListDebug utilities', () => {
         it('should create correct debug info object', () => {
             const visibleRange = { start: 5, end: 15 }
             const totalItems = 100
-            const processedItems = 50
-            const averageItemHeight = 30
+            const processedItems = 25 // Items with measured heights in heightCache
+            const averageItemHeight = 50
 
             const result = createDebugInfo(
                 visibleRange,
@@ -58,8 +58,8 @@ describe('virtualListDebug utilities', () => {
                 startIndex: 5,
                 endIndex: 15,
                 totalItems: 100,
-                processedItems: 50,
-                averageItemHeight: 30
+                processedItems: 25, // Number of items with measured heights
+                averageItemHeight: 50
             })
         })
     })
