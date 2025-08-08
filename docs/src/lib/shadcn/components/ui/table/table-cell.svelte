d0f5b93264170622a62f4e92d2b838d9ab0e4863
Jason Kummerl <jason.h@kummerl.com>
2025-08-08 03:23:35 -0400

    Get things formatted properly

--- docs/src/lib/shadcn/components/ui/table/table-cell.svelte
+++ docs/src/lib/shadcn/components/ui/table/table-cell.svelte
@@ -14,7 +14,7 @@
     bind:this={ref}
     data-slot="table-cell"
     class={cn(
-        'bg-clip-padding p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0',
+        'whitespace-nowrap bg-clip-padding p-2 align-middle [&:has([role=checkbox])]:pr-0',
         className
     )}
     {...restProps}
