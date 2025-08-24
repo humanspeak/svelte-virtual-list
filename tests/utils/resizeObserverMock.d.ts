58b6d006287a7ad0f97f3a74cb4fd742ea3d7556
Jason Kummerl <jason.h@kummerl.com>
2025-08-24 11:24:49 -0400

    Trunk check

--- tests/utils/resizeObserverMock.d.ts
+++ tests/utils/resizeObserverMock.d.ts
@@ -1,9 +1,11 @@
 export declare class ResizeObserverMock implements ResizeObserver {
     private callback
+    /* trunk-ignore(eslint/no-unused-vars) */
     constructor(callback: ResizeObserverCallback)
     observe(): void
     unobserve(): void
     disconnect(): void
+    /* trunk-ignore(eslint/no-unused-vars) */
     emit(entries?: ResizeObserverEntry[]): void
 }
 export default ResizeObserverMock
